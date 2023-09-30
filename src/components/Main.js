977bdf0cbe016ea9d10e0c35668cf0791d45892a
Polina Shapovalova <shapovalova.p.d@gmail.com>
2023-09-30 11:42:01 +0300

    add search

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,22 +1,57 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import Search from "./Search";
 import Card from "./Card";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Main({vacancies}){
 
-    const [countCards, setCountCArds] = useState(5);
+    const [countCards, setCountCards] = useState(5);
+    const cards = vacancies;
+    const [activeCards, setActiveCards] = useState(cards);
+    
+
+
+    const handleClickShowMore = () =>{
+        if (activeCards.length - 5 <= 0){
+            setCountCards(activeCards.length);
+        }
+        else{
+            setCountCards(countCards + 5);
+        }
+    }
+
+    const handleSearch = ({form, position}) =>{
+        console.log({form, position});
+        const activeCardsFilter = cards.filter((item) =>{
+            if (form !== 'No selected' || position !== 'No selected'){
+                if (position === 'No selected'){
+                return item.employment?.name === form && item;
+                } else if (form === 'No selected'){
+                    return item.area?.name === position && item;
+                } else{
+                    return (item.area?.name === position && item.employment?.name === form) ? item : null;
+                }
+            } else{
+                return item;
+            }
+        });
+        console.log(activeCardsFilter)
+        setActiveCards(activeCardsFilter);
+        console.log(activeCards);
+    }
 
 
 
     return(
         <main className="main">
             <h1 className="main__title">List of vacancies</h1>
-            <Search />
+            <Search onSearch={handleSearch}/>
             <ul className="main__list">
-                {vacancies?.map((item, i) => (
+                {activeCards?.map((item, i) => (
                     (i < countCards) ? <Card card={item} key ={i} />  : null)
                 )}
             </ul>
+            <button type="button" className={`main__button  ${countCards === activeCards.length || activeCards.length === 0 ? "main__button_unvisible" : null}`} onClick={handleClickShowMore}>Show more</button>
         </main>
     )
 }
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,28 +1,69 @@
-export default function Search(){
+import { useState } from "react";
+import ArrowIcon from '../images/chevronDown.svg';
+import CloseImage from '../images/close.svg';
 
+export default function Search({onSearch}){
 
+    const [form, setForm] = useState('No selected');
+    const [position, setPosition] = useState('No selected');
+    const [formOpen, setFormOpen] = useState(false);
+    const [positionOpen, setPositionOpen] = useState(false)
 
+    const handleClickForm = (e)=>{
+        e.preventDefault();
+        setFormOpen(!formOpen);
+    }
+
+    const handleClickPosition = (e)=>{
+        e.preventDefault();
+        setPositionOpen(!positionOpen);
+    }
+
+    const handlePositionValue = (value)=>{
+        setPosition(value);
+        setPositionOpen(false);
+    }
+
+    const handleFormValue = (value)=>{
+        setForm(value);
+        setFormOpen(false);
+    }
+
+    const handleClearFilter = (e) =>{
+        e.preventDefault();
+        setForm('No selected');
+        setPosition('No selected');
+        onSearch({form: 'No selected', position: 'No selected'})
+    }
+
+    const handleSearch = (e) =>{
+        e.preventDefault();
+        onSearch({ form, position })
+    }
 
     return (
-        <form className="search">
-            <fieldset className="search__fieldset">
+        <form className="search" typeof="submit">
+            <div className="search__fieldset">
                 <div className="search__input-block">
-                    <label className="search__label" for="form">Form</label>
-                    <select className="search__select search__select_no-selected"  id="form">
-                        <option className="search__option search__option_unvisible ">Not selected</option>
-                        <option className="search__option" value="Full time">Full time</option>
-                        <option className="search__option" value="Half time">Half time</option>
-                        <option className="search__option" value="Part time">Part time</option>
-                    </select>
+                    <label className="search__label">Form</label>
+                    <div onClick={handleClickForm} className={`search__select ${form === 'No selected' ? 'search__select_no-selected' : null}`}>{form}<img src={ArrowIcon} alt="стрелочка" className={`search__icon ${formOpen ? 'search__icon_rotate' : null}`}/></div>
+                    <div className={`search__options ${!formOpen ? "search__options_unvisible" : null}`}>
+                        <div className="search__option" onClick={() => handleFormValue("Полная занятость")}>Полная занятость</div>
+                        <div className="search__option" onClick={() => handleFormValue("Частичная занятость")}>Частичная занятость</div>
+                    </div>
                 </div>
                 <div className="search__input-block">
-                    <label className="search__label" for="position">Position</label>
-                    <select className="search__select search__select_no-selected" id="position">
-                    <option className="search__select_no-selected">Not selected</option>
-                    </select>
+                    <label className="search__label">Position</label>
+                    <div onClick={handleClickPosition} className={`search__select ${position === 'No selected' ? 'search__select_no-selected' : null}`}>{position}<img src={ArrowIcon} alt="стрелочка" className={`search__icon ${positionOpen ? 'search__icon_rotate' : null}`}/></div>
+                    <div className={`search__options ${!positionOpen ? "search__options_unvisible" : null}`}>
+                        <div className="search__option" onClick={() => handlePositionValue("Москва")}>Москва</div>
+                        <div className="search__option" onClick={() => handlePositionValue("Тверь")}>Тверь</div>
+                        <div className="search__option" onClick={() => handlePositionValue("Казань")}>Казань</div>
+                    </div>
                 </div>
-            </fieldset>
-            <button className="search__button" type="button">Search</button>
+            </div>
+            <button className="search__button" type="submit" onClick={handleSearch}>Search</button>
+            <button onClick={handleClearFilter} className={`search__button-clear ${form !== 'No selected' || position !== 'No selected' ? 'search__button-clear_visible' : null}`}><img src={CloseImage} alt='крестик' className="search__icon"/> Clear filters</button>
         </form>
     )
 }
